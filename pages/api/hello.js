07af84f9a803315d451fb3fa49eb2be772b0ac10
broneukr <69394343+broneukr@users.noreply.github.com>
2020-08-08 19:01:18 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,6 +3,8 @@
 export default (req, res) => {
   var d = new Date()
   res.statusCode = 200
-  console.log(d)
+  console.warn(d)
+  console.info({ name: 'brone ukr', date: d })
+  console.info([77,888])
   res.json({ name: 'brone ukr', date: d })
 }
