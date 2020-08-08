5b67c96531d545a527ad40a8eb194e0d266fde07
broneukr <69394343+broneukr@users.noreply.github.com>
2020-08-08 18:53:30 +0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
+  var d = new Date()
   res.statusCode = 200
-  console.log(new Date())
-  res.json({ name: 'brone ukr', user: 'brone ukr' })
+  console.log(d)
+  res.json({ name: 'brone ukr', date: d })
 }
